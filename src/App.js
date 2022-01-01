36a48a93a6c11d6f745d9e5db56b2aa1aa169c52
Collins Owusu <owusucollins51@gmail.com>
2022-01-01 07:39:53 +0000

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-    Hello World
-    </div>
+    <div style={{ textAlign: "center", fontSize: "5rem" }}>Hello World</div>
   );
 }
 
